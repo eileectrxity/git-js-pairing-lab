e184ce916f0cf04be3c6e90178fe29deec022147
Linqing Zhu <72286194+LinqingZ@users.noreply.github.com>
2024-03-28 15:39:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -44,3 +44,36 @@ const multiply = (num1, num2) => {
 console.log(multiply(2, 4))     //returns 8
 console.log(multiply(10, -5))  //returns -50
 console.log(multiply(3, 7.5))   //returns 22.5
+
+
+
+// Question 7:
+const isNegative = (num) => {
+    if (num<0){
+        return true;
+    }else{
+        return false;
+    }
+}
+// console.log(isNegative(3))         //returns false
+// console.log(isNegative(-2))        //returns true
+// console.log(isNegative(Math.PI))   //returns false
+
+// Question 8: a function triangleArea that takes in the height and base of a triangle and returns the area of the triangle.
+const triangleArea = (height, base) => {
+    return height * base /2
+}
+
+// console.log(triangleArea(5, 7))    //returns 17.5
+// console.log(triangleArea(6, 8))    //returns 24
+
+// Quesion 9
+
+const betweenTwentyAndFourty = (num) =>{
+    return (num>20 && num <40) ? true: false;
+
+}
+console.log(betweenTwentyAndFourty(20))      //return false
+console.log(betweenTwentyAndFourty(21))     //return true
+console.log(betweenTwentyAndFourty(39))     //return true
+console.log(betweenTwentyAndFourty(40))    //return false
