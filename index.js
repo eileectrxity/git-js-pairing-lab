a04469c72ed78c16d0570516a30f22bfaa18dbe3
LinqingZ <linqing201@gmail.com>
2024-03-28 13:32:14 -0400

    Solved question 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //Code your solutions in this file
+// Odd questions
+
+// Question 1:
+const fiveToOneHundred = () =>{
+    for(let i = 5; i<=100; i++){
+        console.log(i)
+    }
+}
+fiveToOneHundred()
