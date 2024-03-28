bbb31cc119234acb15c81d3c208afeb172d6f89a
electrxety <er191712@gmail.com>
2024-03-28 13:46:47 -0400

    solved question 2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//Code your solutions in this file
+//Git JS Pairing Lab - Even-Questions Branch by Eileen
+
+//Question 2: Write a function multiplesOfThree that console logs all positive numbers to 100 that is a multiple of 3
+const multiplesOfThree = () => {
+    for (let i = 0; i < 101; i++) {
+        if (i % 3 === 0) {
+            console.log(i)
+        }
+    }
+};
+
+multiplesOfThree();
